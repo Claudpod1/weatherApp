a634c9c473b6209d5964c3f253d1225772c57216
Claud Pod <claudiapollinger.cp@gmail.com>
2019-11-08 19:00:45 -0700

    added components

---
+++ react-weather/src/components/DayCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const DayCard = props => {
+return (
+    <h2> Day Card </h2>
+)
+
+}
+
+export default DayCard; 
+
---
+++ react-weather/src/components/DayDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const DayDetails = props => {
+return (
+    <h2> Day Details </h2>
+)
+
+}
+
+export default DayDetails; 
+
